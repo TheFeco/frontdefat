60a2ddb0733496abb38349a457897697a12aae4d
fespinoza <f.espinoza@whipple.mx>
2022-04-27 11:21:55 -0600

    frist commit

--- dashboard/deportistas.js
+++ dashboard/deportistas.js
@@ -1,8 +1,4 @@
 $(document).ready(function(){
-    // console.log(url.pathname.split('/'));
-    // var local = window.location.pathname.split('/');
-    // console.log(local);
-
     const url = window.location;
     console.log(url.pathname.split('/'));
     const params = new URLSearchParams(url.search)
---
+++ dashboardad/vistas/modalLarge.php
@@ -0,0 +1,30 @@
+<div id="modal-lg" class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+        <div class="container">
+            <div class="row">
+                    <div class="col-lg-12">
+                        <div class="table-responsive">        
+                            <table id="tablaPersonas" class="table table-striped table-bordered table-condensed" style="width:100%">
+                            <thead class="text-center">
+                                <tr>
+                                    <th>Escuela</th>
+                                    <th>Ciclo</th>                                
+                                    <th>Nombre</th>                                
+                                    <th>Apellidos</th>                                
+                                    <th>Funcion</th>
+                                    <th>Deporte</th>
+                                    <th>Rama</th>
+                                    <th>Pruebas</th>
+                                </tr>
+                            </thead>
+                            <tbody id="DataDeportistas">
+                            </tbody>        
+                            </table>                    
+                        </div>
+                    </div>
+            </div>
+        </div> 
+    </div>
+  </div>
+</div>
