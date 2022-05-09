20b2ab49e3f4c1973a863554b4e30581b30fed08
TheFeco <francisco.fcospinoza@gmail.com>
2022-05-09 11:51:08 -0600

    borrando console

--- dashboard/deportistas.js
+++ dashboard/deportistas.js
@@ -1,6 +1,4 @@
 $(document).ready(function(){
     const url = window.location;
-    console.log(url.pathname.split('/'));
     const params = new URLSearchParams(url.search)
-    console.log(params)
 });
